f4261fa40cb62cc6fe9e788d6ea5699a12d3b3cf
Bautista Bevilacqua <bautistabevilacqua@hotmail.com>
2026-03-04 02:34:45 -0300

    feat: add GET by id, PUT and DELETE endpoints for beneficiaries

--- src/routes/beneficiario.routes.ts
+++ src/routes/beneficiario.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   getBeneficiarios,
+  getBeneficiarioById,
   postBeneficiario,
   putBeneficiario,
   deleteBeneficiario,
@@ -9,6 +10,7 @@ import {
 const router = Router();
 
 router.get("/", getBeneficiarios);
+router.get("/:id", getBeneficiarioById);
 router.post("/", postBeneficiario);
 router.put("/:id", putBeneficiario);
 router.delete("/:id", deleteBeneficiario);
