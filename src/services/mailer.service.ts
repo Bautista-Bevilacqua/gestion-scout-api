90a1a9788d31c3895e1c65fc4ae9313f553ef6a5
Bautista Bevilacqua <bautistabevilacqua@hotmail.com>
2026-03-16 22:03:29 -0300

    fix: update mailer service configuration to use SMTP settings for Gmail

--- src/services/mailer.service.ts
+++ src/services/mailer.service.ts
@@ -2,7 +2,9 @@ import nodemailer from "nodemailer";
 
 // Usamos las variables de entorno que vas a configurar en Render
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
